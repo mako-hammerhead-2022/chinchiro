e1b7f30b8bd6ab0dbf5a71ac0abd9f2c631b98c2
tim-dustow <103151914+tim-dustow@users.noreply.github.com>
2022-07-13 16:52:50 +1200

    fix dice load state

--- client/components/Dice.jsx
+++ client/components/Dice.jsx
@@ -1,6 +1,14 @@
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 
 const Dice = () => {
+
+  useEffect(() => {
+    setDice1Value(1)
+    setDice2Value(1)
+    setDice3Value(1)
+    
+    
+  },[]);
 
   const diceImage = ['','/dice1.png','/dice2.png','/dice3.png','/dice4.png','/dice5.png','/dice6.png']
 
@@ -12,16 +20,12 @@ const Dice = () => {
 
   }
 
-  // Set rank of roll
-
-  const scoreRank = ['','1-2-3', 'pisser', 'bust', '1', '2','3','4','5', '6', '4-5-6', '2-2-2', '3-3-3', '4-4-4', '5-5-5', '6-6-6', '1-1-1']
-
   // initial dice value
   const [dice1Value,setDice1Value] = useState(1)
   const [dice2Value,setDice2Value] = useState(1)
   const [dice3Value,setDice3Value] = useState(1)
 
-  const [rollP1,setRollP1] = useState([0,0,0])
+  const [rollP1,setRollP1] = useState([1,1,1])
   
   let dice1 = dice1Value
   let dice2 = dice2Value
@@ -42,15 +46,14 @@ const Dice = () => {
 
   return (
     <div>
-    <div>
-    <h1>Player 1:</h1>
-    <div>
-        <img src={diceImage[rollP1[0]]} alt="Player 2, Dice 1" />
-        <img src={diceImage[rollP1[1]]} alt="Player 2, Dice 1" />
-        <img src={diceImage[rollP1[2]]} alt="Player 2, Dice 1" />
-      </div>
-    <button onClick={rollDiceP1}>ROLL THE DICE</button>
-    </div>
+        <div>
+            <div>
+             <img src={diceImage[rollP1[0]]} alt="Player 2, Dice 1" />
+              <img src={diceImage[rollP1[1]]} alt="Player 2, Dice 1" />
+              <img src={diceImage[rollP1[2]]} alt="Player 2, Dice 1" />
+            </div>
+            <button onClick={rollDiceP1}>ROLL THE DICE</button>
+        </div>
     
     </div>
   )
