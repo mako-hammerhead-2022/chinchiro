57a5772effa90d6604b4a08a44e1274dd58eb363
Vincent Nuku <103153767+VincentNuku@users.noreply.github.com>
2022-07-15 22:15:47 +1200

    began work on rules modal

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,8 @@
 //external dependencies
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
-import React from 'react'
+import React, { useState } from 'react'
+import { Button } from 'react-bootstrap'
 
 import Nav from './Nav'
 import Register from './Register'
@@ -9,27 +10,38 @@ import GameBoard from './GameBoard'
 
 import { useAuth0 } from '@auth0/auth0-react'
 import { cacheUser } from '../auth0-utils'
-
+import AppModal from './widgets/AppModal'
 
 import Player from './Player'
 
-
 function App() {
   cacheUser(useAuth0)
+  const [show, setShow] = useState(false)
+
   const dispatch = useDispatch()
+  function handleShow() {
+    setShow(true)
+  }
+
   return (
     <>
       <Router>
         <Nav />
+        <Button variant="primary" onClick={handleShow}>
+          Launch demo modal
+        </Button>
+
         <button onClick={() => dispatch({ type: 'ROTATE_DEALER' })}>
           Rotate Dealer
         </button>
         <button onClick={() => dispatch({ type: 'START_GAME' })}>
           START GAME
         </button>
+        <AppModal onClose={() => setShow(false)} show={show} />
+
         <Routes>
-          <Route path='/' element={<GameBoard />} />
-          <Route path='register' element={<Register />} />
+          <Route path="/" element={<GameBoard />} />
+          <Route path="register" element={<Register />} />
         </Routes>
       </Router>
     </>
