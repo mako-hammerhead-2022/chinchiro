20f28f2740e253717e075ffb57ea4e7f805467bb
Vincent Nuku <103153767+VincentNuku@users.noreply.github.com>
2022-07-19 12:35:50 +1200

    gameover for user

--- client/components/Player.jsx
+++ client/components/Player.jsx
@@ -2,61 +2,67 @@ import React from 'react'
 
 import Counter from './Counter'
 import Dice from './Dice'
-import { useDispatch , useSelector } from 'react-redux'
+import PlayerDead from './widgets/PlayerDead'
+import { useDispatch, useSelector } from 'react-redux'
 import { addToWallet, removeFromWallet } from '../reducers/players'
 
-
 function Player(props) {
-
   const players = useSelector((state) => state.players)
-
-  
-  
   const dispatch = useDispatch()
-  
+
   const PlayerWin = () => {
     const currentDealer = players.find((player) => player.isDealer)
-    
     dispatch(addToWallet(props.id, calcResults(props.bet, props.result)))
-    dispatch(removeFromWallet(currentDealer.id, calcResults(props.bet, props.result)))
+    dispatch(
+      removeFromWallet(currentDealer.id, calcResults(props.bet, props.result))
+    )
   }
 
   const DealerWin = () => {
     const currentDealer = players.find((player) => player.isDealer)
-    console.log(currentDealer.result)
-    dispatch(addToWallet(currentDealer.id, calcResults(props.bet, currentDealer.result)))
-    dispatch(removeFromWallet(props.id, calcResults(props.bet, currentDealer.result)))
+    dispatch(
+      addToWallet(
+        currentDealer.id,
+        calcResults(props.bet, currentDealer.result)
+      )
+    )
+    dispatch(
+      removeFromWallet(props.id, calcResults(props.bet, currentDealer.result))
+    )
   }
 
   return (
     <div>
-      <div className="card-top">
-        <div className="user-info">
-          <img
-            className="avatar-container"
-            src={props.avatar}
-            alt="player avatar"
-          ></img>
-          <h1>{props.name}</h1>
-        </div>
-        <div className="dice-result">{props.result}</div>
-      </div>
-      <div>
-        <h2>Wallet: {props.wallet}</h2>
-      </div>
-      <Dice id={props.id} dice={props.dice} />
-      {/* or dice? */}
-      {props.isDealer ? (
-        <h1>YOU ARE THE DEALER</h1>
-      ) : (<>
-        <Counter id={props.id} bet={props.bet} />
-        <button onClick={PlayerWin}>
-          WINNER
-        </button>
-        <button onClick={DealerWin}>
-          LOSER
-        </button>
-        </>
+      {props.wallet !== 0 ? (
+        <React.Fragment>
+          <div className="card-top">
+            <div className="user-info">
+              <img
+                className="avatar-container"
+                src={props.avatar}
+                alt="player avatar"
+              ></img>
+              <h1>{props.name}</h1>
+            </div>
+            <div className="dice-result">{props.result}</div>
+          </div>
+          <div>
+            <h2>Wallet: {props.wallet}</h2>
+          </div>
+          <Dice id={props.id} dice={props.dice} />
+          {/* or dice? */}
+          {props.isDealer ? (
+            <h1>YOU ARE THE DEALER</h1>
+          ) : (
+            <>
+              <Counter id={props.id} bet={props.bet} />
+              <button onClick={PlayerWin}>WINNER</button>
+              <button onClick={DealerWin}>LOSER</button>
+            </>
+          )}
+        </React.Fragment>
+      ) : (
+        <PlayerDead username={props.name} avatar={props.avatar} />
       )}
     </div>
   )
@@ -76,7 +82,7 @@ function calcResults(bet, result) {
     case (result = 'x5'):
       return bet * 5
     case (result = '-x2'):
-      return bet * 2 
+      return bet * 2
     case (result = 'x2'):
       return bet * 2
     case (result = 'pisser'):
